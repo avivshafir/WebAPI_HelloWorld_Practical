a45e46a367a85a15efa6ffe8b85f79397ba3daa9
Aviv Shafir <ashafir20@gmail.com>
2014-09-16 14:05:16 +0300

    init

---
+++ HellowWorld_WebAPI_Practical_WEB_API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HellowWorld_WebAPI_Practical_WEB_API.WebApiApplication" Language="C#" %>
---
+++ HellowWorld_WebAPI_Practical_WEB_API/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace HellowWorld_WebAPI_Practical_WEB_API.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
